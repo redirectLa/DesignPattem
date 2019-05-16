97ea8ef9f3da50b6a7045934f1cf785fec383828
傲 鲁 <2551430130@qq.com>
2019-05-16 18:41:39 +0800

    添加项目文件。

---
+++ FactoryPattem/OperationFactory.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace FactoryPattem
+{
+    /// <summary>
+    /// 计算器操作简单工厂
+    /// </summary>
+    public class OperationFactory
+    {
+        public static Operation GetOperation(string operate)
+        {
+            Operation operation = null;
+            switch (operate)
+            {
+                case "+":
+                    operation = new OperationAdd();
+                    break;
+                case "-":
+                    operation = new OperationSub();
+                    break;
+                case "*":
+                    operation = new OperationMul();
+                    break;
+                case "/":
+                    operation = new OperationDiv();
+                    break;
+                default: throw new NullReferenceException("暂不支持此操作！");
+            }
+            return operation;
+        }
+
+        public class Operation
+        {
+            public double NumberA { get; set; }
+            public double NumberB { get; set; }
+
+            public virtual double GetResult()
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// 加法操作类
+        /// </summary>
+        public class OperationAdd : Operation
+        {
+            public override double GetResult()
+            {
+                return NumberA + NumberB;
+            }
+        }
+
+        /// <summary>
+        /// 减法操作类
+        /// </summary>
+        public class OperationSub : Operation
+        {
+            public override double GetResult()
+            {
+                return NumberA - NumberB;
+            }
+        }
+
+        /// <summary>
+        /// 乘法操作类
+        /// </summary>
+        public class OperationMul : Operation
+        {
+            public override double GetResult()
+            {
+                return NumberA * NumberB;
+            }
+        }
+
+        /// <summary>
+        /// 除法操作类
+        /// </summary>
+        public class OperationDiv : Operation
+        {
+            public override double GetResult()
+            {
+                if (NumberB == 0) throw new Exception("被除数不能为0！");
+                return NumberA / NumberB;
+            }
+        }
+    }
+}
---
+++ FactoryPattem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FactoryPattem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                #region  简单工厂（计算器）
+                Console.WriteLine("请输入数字A");
+                var numberA = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("请输入运算符号(+、-、*、/)");
+                var operate = Console.ReadLine();
+                Console.WriteLine("请输入数字B");
+                var numberB = Convert.ToDouble(Console.ReadLine());
+
+                var operation = OperationFactory.GetOperation(operate);
+                operation.NumberA = numberA;
+                operation.NumberB = numberB;
+                Console.WriteLine(operation.GetResult());
+                #endregion
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"遇到异常,ErrorMsg：{e.Message}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
