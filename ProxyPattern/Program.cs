a8d66df166ea9c3edf02197fef9e748aec4dac06
傲 鲁 <2551430130@qq.com>
2019-05-20 17:01:58 +0800

    增加工厂方法模式、建造者模式、模板方法模式、代理模式、外观模式、原型模式、装饰模式

---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //概念:1.为其它对象提供一种代理以控制对这个对象的访问(DP)
+                Proxy proxy = new Proxy();
+                proxy.Request();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"遇到异常,ErrorMsg：{e.Message}");
+                Console.ReadLine();
+            }
+        }
+
+        abstract class Subject
+        {
+            public abstract void Request();
+        }
+
+        class RealSubject : Subject
+        {
+            public override void Request()
+            {
+                Console.WriteLine("真实的请求");
+            }
+        }
+
+        class Proxy : Subject
+        {
+            RealSubject realSubject;
+            public override void Request()
+            {
+                if (realSubject == null)
+                {
+                    realSubject = new RealSubject();
+                }
+
+                realSubject.Request();
+            }
+        }
+    }
+}
