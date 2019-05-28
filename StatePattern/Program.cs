31126768ae6fe11aa5a6b72882e85836824f9950
傲 鲁 <2551430130@qq.com>
2019-05-28 19:56:44 +0800

    1

---
+++ StatePattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //状态模式：当一个对象的内在状态改变时允许改变其行为，这个对象看起来像是改变了其类。
+            //状态模式主要是解决的是当控制一个对象状态改变的条件表达式过于复杂的情况，把状态判断逻辑转移到表示不同状态一系类中，
+            //可以把复杂的判断逻辑简化
+
+
+            Console.ReadLine();
+        }
+    }
+}
