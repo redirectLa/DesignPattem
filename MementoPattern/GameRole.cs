3d698f832d6554a996a515234cccaab037ba6a50
傲 鲁 <2551430130@qq.com>
2019-05-31 16:43:48 +0800

    添加备忘录模式

---
+++ MementoPattern/GameRole.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MementoPattern
+{
+    public class GameRole
+    {
+        public int Vitality { get; set; }
+
+        public int Attack { get; set; }
+
+        public int Defense { get; set; }
+
+        public void InitState()
+        {
+            Vitality = 100;
+            Attack = 100;
+            Defense = 100;
+        }
+
+        public RoleStateMemento SaveState()
+        {
+            return new RoleStateMemento(Vitality, Attack, Defense);
+        }
+
+        public void RecoveryState(RoleStateMemento roleStateMemento)
+        {
+            this.Attack = roleStateMemento.Attack;
+            this.Defense = roleStateMemento.Defense;
+            this.Vitality = roleStateMemento.Vitality;
+        }
+
+        public void Fight()
+        {
+            this.Attack = 1;
+            this.Defense = 1;
+            this.Vitality = 1;
+            Console.WriteLine("大战boss，属性降低到最低点了..");
+        }
+
+        public void StateDisplay()
+        {
+            Console.WriteLine($"当前状态：生命力:{Vitality},攻击力:{Attack},防御力:{Defense}");
+        }
+
+    }
+
+    public class RoleStateMemento
+    {
+        public RoleStateMemento(int vitality, int attack, int defense)
+        {
+            Vitality = vitality;
+            Attack = attack;
+            Defense = defense;
+        }
+
+        /// <summary>
+        /// 生命力
+        /// </summary>
+        public int Vitality { get; set; }
+
+        /// <summary>
+        /// 攻击力
+        /// </summary>
+        public int Attack { get; set; }
+
+        /// <summary>
+        /// 防御力
+        /// </summary>
+        public int Defense { get; set; }
+
+    }
+
+    public class RoleStateCaretaker
+    {
+        private RoleStateMemento roleStateMemento;
+
+        public RoleStateMemento RoleStateMemento
+        {
+            get => roleStateMemento;
+            set => roleStateMemento = value;
+        }
+    }
+}
---
+++ MementoPattern/Originator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MementoPattern
+{
+    //发起
+    public class Originator
+    {
+        private string state { get; set; }
+
+        public string State
+        {
+            get => state;
+            set => state = value;
+        }
+
+        public Memento CreateMemento()
+        {
+            return new Memento(state);
+        }
+
+        public void SetMemento(Memento memento)
+        {
+            state = memento.State;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"State:{state}");
+        }
+    }
+
+    //备忘录
+    public class Memento
+    {
+        public string State { get; set; }
+
+        public Memento(string state)
+        {
+            State = state;
+        }
+    }
+
+    //管理者
+    public class Caretaker
+    {
+        public Memento Memento { get; set; }
+
+    }
+}
