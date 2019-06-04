91676bf020c62b2bd0a7e14b04b0a21e90266459
傲 鲁 <2551430130@qq.com>
2019-06-04 14:54:44 +0800

    增加迭代器模式

---
+++ LteratorPattern/Iterator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LteratorPattern
+{
+    public abstract class Iterator
+    {
+        public abstract object First();
+        public abstract object Next();
+        public abstract bool IsDone();
+        public abstract object CurrentItem();
+    }
+
+    public abstract class Aggregate
+    {
+        public abstract Iterator CreateIterator();
+    }
+
+    public class ConcreteIterator : Iterator
+    {
+        private ConcreteAggregate _concreteAggregate;
+        private int current = 0;
+        public ConcreteIterator(ConcreteAggregate concreteAggregate)
+        {
+            this._concreteAggregate = concreteAggregate;
+        }
+
+        public override object First()
+        {
+            return _concreteAggregate[0];
+        }
+
+        public override object Next()
+        {
+            object ret = null;
+            current++;
+            if (current < _concreteAggregate.Count)
+            {
+                ret = _concreteAggregate[current];
+            }
+            return ret;
+        }
+
+        public override bool IsDone()
+        {
+            return current >= _concreteAggregate.Count;
+        }
+
+        public override object CurrentItem()
+        {
+            return _concreteAggregate[current];
+        }
+    }
+
+    public class ConcreteAggregate : Aggregate
+    {
+        private IList<object> items = new List<object>();
+        public override Iterator CreateIterator()
+        {
+            return new ConcreteIterator(this);
+        }
+        public int Count
+        {
+            get { return items.Count; }
+        }
+
+        public object this[int index]
+        {
+            get { return items[index]; }
+            set { items.Insert(index, value); }
+        }
+    }
+
+}
