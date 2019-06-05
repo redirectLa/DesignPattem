7180a13e6e7303efc77084bdffd8d854ad320481
傲 鲁 <2551430130@qq.com>
2019-06-05 10:56:38 +0800

    增加单例模式

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,37 @@
+﻿namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton singleton = null;
+        private Singleton() { }
+
+        private static readonly object lockStr = new object();
+        public static Singleton GetInstance()
+        {
+            if (singleton == null)
+            {
+                lock (lockStr)
+                {
+                    if (singleton == null)
+                    {
+                        singleton = new Singleton();
+                    }
+                }
+
+            }
+            return singleton;
+        }
+    }
+
+    public sealed class StaticSingleton
+    {
+        private static readonly StaticSingleton singleton = new StaticSingleton();
+        private StaticSingleton() { }
+        public static StaticSingleton GetInstance()
+        {
+            return singleton;
+        }
+
+    }
+
+}
