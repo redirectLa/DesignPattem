6b6b2ea940132f1a7a4f52b809bb6c38e253e0f5
傲 鲁 <2551430130@qq.com>
2019-06-06 11:34:02 +0800

    增加桥接模式

---
+++ BridgePattern/HandsetSoft.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgePattern
+{
+    //手机软件
+    public abstract class HandsetSoft
+    {
+        public abstract void Run();
+    }
+
+    public class HandsetGame : HandsetSoft
+    {
+        public override void Run()
+        {
+            Console.WriteLine("运行手机游戏");
+        }
+    }
+
+    public class HandsetAddressList : HandsetSoft
+    {
+        public override void Run()
+        {
+            Console.WriteLine("运行手机通讯录");
+        }
+    }
+
+    //手机品牌
+    abstract class HandsetBrand
+    {
+        protected HandsetSoft HandsetSoft;
+
+        protected HandsetBrand(HandsetSoft handsetSoft)
+        {
+            this.HandsetSoft = handsetSoft;
+        }
+
+        public abstract void Run();
+    }
+
+    class HandsetBrandM : HandsetBrand
+    {
+        public override void Run()
+        {
+            base.HandsetSoft.Run();
+        }
+
+        public HandsetBrandM(HandsetSoft handsetSoft) : base(handsetSoft)
+        {
+        }
+    }
+
+    class HandsetBrandN : HandsetBrand
+    {
+        public override void Run()
+        {
+            base.HandsetSoft.Run();
+        }
+
+        public HandsetBrandN(HandsetSoft handsetSoft) : base(handsetSoft)
+        {
+        }
+    }
+}
---
+++ BridgePattern/Implementor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgePattern
+{
+    public abstract class Implementor
+    {
+        public abstract void Operation();
+    }
+
+    public class ConcreteImplementorA : Implementor
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteImplementorA");
+        }
+    }
+
+    public class ConcreteImplementorB : Implementor
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteImplementorB");
+        }
+    }
+
+    public class Abstraction
+    {
+        protected Implementor Implementor;
+
+        protected Abstraction(Implementor implementor)
+        {
+            this.Implementor = implementor;
+        }
+
+        public virtual void Operation()
+        {
+            Implementor.Operation();
+        }
+    }
+
+    public class RefinedAbstraction : Abstraction
+    {
+        public RefinedAbstraction(Implementor implementor) : base(implementor)
+        {
+        }
+
+        public override void Operation()
+        {
+            Implementor.Operation();
+        }
+    }
+}
