119c36d7af1d31e52090461f73aae501406ad544
傲 鲁 <2551430130@qq.com>
2019-06-06 14:02:27 +0800

    增加命令模式

---
+++ CommandPattern/Barbecuer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern
+{
+    public class Barbecuer
+    {
+        public void BakeMutton()
+        {
+            Console.WriteLine("烤羊肉串");
+        }
+
+        public void BakeChickenWing()
+        {
+            Console.WriteLine("烤鸡翅");
+        }
+    }
+
+    public abstract class Command
+    {
+        protected Barbecuer receiver;
+
+        protected Command(Barbecuer barbecuer)
+        {
+            this.receiver = barbecuer;
+        }
+
+        public abstract void ExcuteCommand();
+    }
+
+
+    public class BakeMuttonCommand : Command
+    {
+        public BakeMuttonCommand(Barbecuer barbecuer) : base(barbecuer)
+        {
+        }
+
+        public override void ExcuteCommand()
+        {
+            receiver.BakeMutton();
+        }
+    }
+
+    public class BakeChickenWingCommand : Command
+    {
+        public BakeChickenWingCommand(Barbecuer barbecuer) : base(barbecuer)
+        {
+        }
+
+        public override void ExcuteCommand()
+        {
+            receiver.BakeChickenWing();
+        }
+    }
+
+    public class Waiter
+    {
+        private IList<Command> commands = new List<Command>();
+
+        public void SetOrder(Command command)
+        {
+            if (command.ToString() == "CommandPattern.BakeChickenWingCommand")
+            {
+                Console.WriteLine("服务员：不好意思，鸡翅卖完了！");
+            }
+            else
+            {
+                commands.Add(command);
+                Console.WriteLine("增加订单：" + command.ToString() + "时间:" + DateTime.Now);
+            }
+        }
+
+        public void CancelOrder(Command command)
+        {
+            commands.Remove(command);
+            Console.WriteLine("取消订单：" + command.ToString() + "时间:" + DateTime.Now);
+        }
+
+        //执行所有命令
+        public void Notify()
+        {
+            foreach (var command in commands)
+            {
+                command.ExcuteCommand();
+            }
+        }
+    }
+}
---
+++ CommandPattern/Command.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Commands
+{
+    public abstract class Command
+    {
+        protected Barbecuer Barbecuer;
+        protected Command(Barbecuer barbecuer)
+        {
+            this.Barbecuer = barbecuer;
+        }
+
+        public abstract void ExcuteAction();
+    }
+
+    public class Barbecuer
+    {
+        public void Action()
+        {
+            Console.WriteLine("执行方法");
+        }
+    }
+
+    public class ConcreteCommand : Command
+    {
+        public ConcreteCommand(Barbecuer barbecuer) : base(barbecuer)
+        {
+        }
+
+        public override void ExcuteAction()
+        {
+            Barbecuer.Action();
+        }
+    }
+
+    public class Invoker
+    {
+        private Command Command;
+
+        public Invoker(Command command)
+        {
+            this.Command = command;
+        }
+
+        public void ExcuteCommand()
+        {
+            Command.ExcuteAction();
+        }
+    }
+
+}
