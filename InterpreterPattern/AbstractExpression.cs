babc4bee50823d3582b1400e1802f3c03f9180aa
傲 鲁 <2551430130@qq.com>
2019-06-11 16:02:01 +0800

    添加解释器模式

---
+++ InterpreterPattern/AbstractExpression.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace InterpreterPattern
+{
+    //抽象解释器
+    public abstract class AbstractExpression
+    {
+        public abstract void Interpret(Context context);
+    }
+
+
+    public class TerminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("终端解释器");
+        }
+    }
+
+    public class NonTerminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("非终端解释器");
+        }
+    }
+
+    public class Context
+    {
+        private string input { get; set; }
+
+        public string Input
+        {
+            get { return input; }
+            set { input = value; }
+        }
+
+        private string output { get; set; }
+
+        public string Output
+        {
+            get { return output; }
+            set { output = value; }
+        }
+
+
+    }
+
+}
---
+++ InterpreterPattern/PlayContext.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterpreterPattern
+{
+    //演奏内容
+    public class PlayContext
+    {
+        //演奏文本
+        private string text { get; set; }
+
+        public string PlayText
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+
+    }
+
+    //表达式类
+    public abstract class Expression
+    {
+        //解释器
+        public void Interpreter(PlayContext context)
+        {
+            if (context.PlayText.Length == 0)
+            {
+                return;
+            }
+
+            string playKey = context.PlayText.Substring(0, 1);
+            context.PlayText = context.PlayText.Substring(2);
+            double playValue = Convert.ToDouble(context.PlayText.Substring(0, context.PlayText.IndexOf(" ")));
+            context.PlayText = context.PlayText.Substring(context.PlayText.IndexOf(" ") + 1);
+            Excute(playKey, playValue);
+        }
+        //执行
+        public abstract void Excute(string key, double value);
+    }
+
+    public class Note : Expression
+    {
+        public override void Excute(string key, double value)
+        {
+            string note = "";
+            switch (key)
+            {
+                case "C":
+                    note = "1";
+                    break;
+                case "D":
+                    note = "2";
+                    break;
+                case "E":
+                    note = "3";
+                    break;
+                case "F":
+                    note = "4";
+                    break;
+                case "G":
+                    note = "5";
+                    break;
+                case "A":
+                    note = "6";
+                    break;
+                case "B":
+                    note = "7";
+                    break;
+            }
+
+            Console.WriteLine(note);
+        }
+
+
+    }
+
+    public class Scale : Expression
+    {
+        public override void Excute(string key, double value)
+        {
+            string scale = "";
+            switch (Convert.ToInt32(value))
+            {
+                case 1:
+                    scale = "低音";
+                    break;
+                case 2:
+                    scale = "中音";
+                    break;
+                case 3:
+                    scale = "高音";
+                    break;
+            }
+            Console.WriteLine(scale);
+        }
+    }
+}
