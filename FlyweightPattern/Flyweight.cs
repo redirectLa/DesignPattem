126ab043bbf9c9e116597781d367b68b6494dce8
傲 鲁 <2551430130@qq.com>
2019-06-11 11:37:33 +0800

    添加享元模式

---
+++ FlyweightPattern/Flyweight.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace FlyweightPattern
+{
+    public abstract class Flyweight
+    {
+        public abstract void Operation(int extrinsicstate);
+    }
+
+
+
+    public class ConcreteFlyweight : Flyweight
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("具体的Flyweight：" + extrinsicstate);
+
+        }
+    }
+
+    public class UnsharedConcreteFlyweight : Flyweight
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("不共享的具体的Flyweight：" + extrinsicstate);
+
+        }
+    }
+
+    //享元工厂
+    public class FlyweightFactory
+    {
+        private Hashtable flywegihts = new Hashtable();
+
+        public FlyweightFactory()
+        {
+            flywegihts.Add("X", new ConcreteFlyweight());
+            flywegihts.Add("Y", new ConcreteFlyweight());
+            flywegihts.Add("Z", new ConcreteFlyweight());
+        }
+
+        public Flyweight GetFlyweight(string key)
+        {
+            return (Flyweight)flywegihts[key];
+        }
+    }
+}
---
+++ FlyweightPattern/Website.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+
+namespace FlyweightPattern
+{
+    //抽象网站类
+    public abstract class WebSite
+    {
+        public abstract void Use(User user);
+    }
+
+    //用户
+    public class User
+    {
+        private string name;
+
+        public User(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+
+    //具体网站类
+    public class ConcreteWebSite : WebSite
+    {
+        private string name;
+
+        public ConcreteWebSite(string name)
+        {
+            this.name = name;
+        }
+
+        public override void Use(User user)
+        {
+            Console.WriteLine("网站分类：{0}，用户：{1}", name, user.Name);
+        }
+    }
+
+    public class WebSiteFactory
+    {
+        private Hashtable webSites = new Hashtable();
+
+        public WebSite GetWebSite(string key)
+        {
+            if (!webSites.Contains(key))
+                webSites.Add(key, new ConcreteWebSite(key));
+            return (WebSite)webSites[key];
+        }
+
+        public int GetWebSiteCount()
+        {
+            return webSites.Count;
+        }
+    }
+}
